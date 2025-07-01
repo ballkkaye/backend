454bc117b28ec877aab3b932adbf9ffe58e02526
hahamik <dlapdl0615@gmail.com>
2025-07-01 14:35:37 +0900

    옵션 박스용 팀 목록

--- src/main/java/com/example/ballkkaye/team/TeamController.java
+++ src/main/java/com/example/ballkkaye/team/TeamController.java
@@ -1,10 +1,22 @@
 package com.example.ballkkaye.team;
 
+import com.example.ballkkaye._core.util.Resp;
+import com.example.ballkkaye.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class TeamController {
     private final TeamService teamService;
+    private final UserRepository userRepository; // 지워야함
+
+    // 팀 목록 << 옵션박스용
+    @GetMapping("/api/teams")
+    public ResponseEntity<?> getTeams() {
+        var respDTO = teamService.getTeams();
+        return Resp.ok(respDTO);
+    }
 }
--- src/main/java/com/example/ballkkaye/team/TeamService.java
+++ src/main/java/com/example/ballkkaye/team/TeamService.java
@@ -3,8 +3,20 @@ package com.example.ballkkaye.team;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class TeamService {
     private final TeamRepository teamRepository;
+
+    public List<TeamResponse.ListDTO> getTeams() {
+        List<Team> teams = teamRepository.findAll();
+        List<TeamResponse.ListDTO> respDTO = new ArrayList<>();
+        for (Team team : teams) {
+            respDTO.add(new TeamResponse.ListDTO(team.getId(), team.getTeamName()));
+        }
+        return respDTO;
+    }
 }
