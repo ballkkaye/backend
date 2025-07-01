47dba685de6849f3c7b433e8bf9c83446ae3aa05
hahamik <dlapdl0615@gmail.com>
2025-07-01 14:16:04 +0900

    게시글 댓글 좋아요 등록 기능 추가

--- src/main/java/com/example/ballkkaye/board/reply/like/BoardReplyLikeResponse.java
+++ src/main/java/com/example/ballkkaye/board/reply/like/BoardReplyLikeResponse.java
@@ -1,8 +1,14 @@
 package com.example.ballkkaye.board.reply.like;
 
 public class BoardReplyLikeResponse {
+    @Data
     public static class SaveDTO {
+        private BoardReplyLike boardReplyLike;
+        private Integer boardReplyLikeCount;
+
         public SaveDTO(BoardReplyLike boardReplyLike, Integer boardLikeCount) {
+            this.boardReplyLike = boardReplyLike;
+            this.boardReplyLikeCount = boardLikeCount;
         }
     }
 }
